9812884846097a297aff69c908fd2185ba4a38c8
JennaFicula <jenna.ficula@gmail.com>
2016-09-19 18:19:18 -0400

    Jenna Ficula, Lab 3, 9/19/16
    updated

--- Lab3_Database.sql
+++ Lab3_Database.sql
@@ -1,39 +1,52 @@
+--Jenna Ficula
+--Lab 3
+--9/19/16
+
+--#1
 SELECT ordnum, totalUSD
 FROM orders;
 
+--#2
 SELECT name, city
 FROM agents
 WHERE name = 'Smith';
 
+--#3
 SELECT pid, name, priceUSD
 FROM products
 WHERE quantity > 201000;
 
+--#4
 SELECT name, city
 FROM customers
 WHERE city = 'Duluth';
 
+--#5
 SELECT name
 FROM agents 
 WHERE (city != 'Duluth' 
-AND   city != 'New York');
+	AND   city != 'New York');
 
+--#6
 SELECT *
 FROM products
 WHERE (city != 'Dallas' 
-AND   city != 'Duluth')
-AND   priceUSD >= 1;
+	AND   city != 'Duluth')
+	AND   priceUSD >= 1;
 
+--#7
 SELECT *
 FROM orders
 WHERE (mon = 'feb' 
-OR   mon = 'mar');
+	OR   mon = 'mar');
 
+--#8
 SELECT *
 FROM orders
 WHERE (mon = 'feb' 
-AND   totalUSD >= 600);
+	AND   totalUSD >= 600);
 
+--#9
 SELECT *
 FROM customers
 WHERE cid = 'c005';
